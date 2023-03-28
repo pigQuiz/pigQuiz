670e1bc03f0b4bedf0d33a113fd21dd28f0e11e2
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-28 18:01:59 +0900

    initial commit

--- 돼지테스트/test.js
+++ 돼지테스트/test.js
@@ -34,11 +34,11 @@ const imgs = [
 ];
 
 
-// function showimg () {
-//     let randomImg = Math.floor(Math.random() * imgs.length );
-//     let randomBox = document.getElementById("foodimg");
+function showimg () {
+    let randomImg = Math.floor(Math.random() * imgs.length );
+    let randomBox = document.getElementById("foodimg");
 
-//     randomBox.src = `${randomImg}`;
-//     const foodImge = document.querySelector(".imgbox");
-//     foodImge.appendChild(krimg);
-// }
+    randomBox.src = `${randomImg}`;
+    const foodImge = document.querySelector(".imgbox");
+    foodImge.appendChild(krimg);
+}
