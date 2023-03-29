ac3520118f3c1642c0641bb2c4e7a4eddbecd253
oslob99 <jkh991116@naver.com>
2023-03-29 20:14:08 +0900

    중간중간중간 점검

--- kh/main.js
+++ kh/main.js
@@ -2,8 +2,20 @@ import * as timer from './timermain.js';
 import * as foodList from '../돼지테스트/foodlist1.js';
 
 const gameData = timer.makeGameData();
+let count = 0;
+const $answer = document.getElementById('answer');
+// const a = foodList.rdimg();
+// foodList.imgMatch(foodList.selectedCourse);
 
-timer.startGameBtn(gameData);
+
+// while(count < 0){
+    timer.startGameBtn(gameData);
+
+    
+//     count++;
+// }
+
+
 // foodList.Enter();
 // foodList.levelSelecter();
 // foodList.imgMatch();
