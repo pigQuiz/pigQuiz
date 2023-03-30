8695eac1f3bc95eee219c3ac7a0c41a9cdc30e33
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-30 18:58:14 +0900

    마지막 커밋

--- 돼지테스트/main.js
+++ 돼지테스트/main.js
@@ -1,386 +1,386 @@
 const korea = [{
-  food: '삼겹살',
-  img: './photo/삼겹살.jpg'
-},
+    food: '삼겹살',
+    img: './photo/삼겹살.jpg'
+  },
 
-{
-  food: '순대국',
-  img: './photo/순대국.jpg'
-},
+  {
+    food: '순대국',
+    img: './photo/순대국.jpg'
+  },
 
-{
-  food: '치킨',
-  img: './photo/치킨.jpg'
-},
+  {
+    food: '치킨',
+    img: './photo/치킨.jpg'
+  },
 
-{
-  food: '뼈해장국',
-  img: './photo/뼈해장국.jpg'
-},
+  {
+    food: '뼈해장국',
+    img: './photo/뼈해장국.jpg'
+  },
 
-{
-  food: '제육볶음',
-  img: './photo/제육볶음.jpg'
-},
+  {
+    food: '제육볶음',
+    img: './photo/제육볶음.jpg'
+  },
 
-{
-  food: '잡채',
-  img: './photo/잡채.jpg'
-},
+  {
+    food: '잡채',
+    img: './photo/잡채.jpg'
+  },
 
-{
-  food: '쌈밥',
-  img: './photo/쌈밥.jpg'
-},
+  {
+    food: '쌈밥',
+    img: './photo/쌈밥.jpg'
+  },
 
-{
-  food: '냉면',
-  img: './photo/냉면.jpg'
-},
+  {
+    food: '냉면',
+    img: './photo/냉면.jpg'
+  },
 
-{
-  food: '계란말이',
-  img: './photo/계란말이.jpg'
-},
+  {
+    food: '계란말이',
+    img: './photo/계란말이.jpg'
+  },
 
-{
-  food: '곱창',
-  img: './photo/곱창.jpg'
-},
+  {
+    food: '곱창',
+    img: './photo/곱창.jpg'
+  },
 
-{
-  food: '갈비찜',
-  img: './photo/갈비찜.jpg'
-},
+  {
+    food: '갈비찜',
+    img: './photo/갈비찜.jpg'
+  },
 
-{
-  food: '보쌈',
-  img: './photo/보쌈.jpg'
-},
+  {
+    food: '보쌈',
+    img: './photo/보쌈.jpg'
+  },
 
-{
-  food: '족발',
-  img: './photo/족발.jpg'
-},
+  {
+    food: '족발',
+    img: './photo/족발.jpg'
+  },
 
-{
-  food: '닭갈비',
-  img: './photo/닭갈비.jpg'
-},
+  {
+    food: '닭갈비',
+    img: './photo/닭갈비.jpg'
+  },
 
-{
-  food: '닭강정',
-  img: './photo/닭강정.jpg'
-},
+  {
+    food: '닭강정',
+    img: './photo/닭강정.jpg'
+  },
 
-{
-  food: '백숙',
-  img: './photo/백숙.jpg'
-},
+  {
+    food: '백숙',
+    img: './photo/백숙.jpg'
+  },
 
-{
-  food: '물회',
-  img: './photo/물회.jpg'
-},
+  {
+    food: '물회',
+    img: './photo/물회.jpg'
+  },
 
-{
-  food: '재첩국',
-  img: './photo/재첩국.jpg'
-},
+  {
+    food: '재첩국',
+    img: './photo/재첩국.jpg'
+  },
 
-{
-  food: '콩나물국',
-  img: './photo/콩나물국.jpg'
-},
+  {
+    food: '콩나물국',
+    img: './photo/콩나물국.jpg'
+  },
 
-{
-  food: '갈비탕',
-  img: './photo/갈비탕.jpg'
-},
+  {
+    food: '갈비탕',
+    img: './photo/갈비탕.jpg'
+  },
 
-{
-  food: '김치찌개',
-  img: './photo/김치찌개.jpg'
-},
+  {
+    food: '김치찌개',
+    img: './photo/김치찌개.jpg'
+  },
 
-{
-  food: '고추장찌개',
-  img: './photo/고추장찌개.jpg'
-},
+  {
+    food: '고추장찌개',
+    img: './photo/고추장찌개.jpg'
+  },
 
-{
-  food: '부대찌개',
-  img: './photo/부대찌개.jpg'
-},
+  {
+    food: '부대찌개',
+    img: './photo/부대찌개.jpg'
+  },
 
 ];
 
 const China = [
 
-{
-  food: '마라샹궈',
-  img: './photo/마라샹궈.jpg'
-},
+  {
+    food: '마라샹궈',
+    img: './photo/마라샹궈.jpg'
+  },
 
-{
-  food: '우육면',
-  img: './photo/우육면.jpg'
-},
+  {
+    food: '우육면',
+    img: './photo/우육면.jpg'
+  },
 
-{
-  food: '깐쇼새우',
-  img: './photo/깐쇼새우.jpg'
-},
+  {
+    food: '깐쇼새우',
+    img: './photo/깐쇼새우.jpg'
+  },
 
-{
-  food: '꽃빵',
-  img: './photo/꽃빵.jpg'
-},
+  {
+    food: '꽃빵',
+    img: './photo/꽃빵.jpg'
+  },
 
-{
-  food: '난자완스',
-  img: './photo/난자완스.jpg'
-},
+  {
+    food: '난자완스',
+    img: './photo/난자완스.jpg'
+  },
 
-{
-  food: '동파육',
-  img: './photo/동파육.jpg'
-},
+  {
+    food: '동파육',
+    img: './photo/동파육.jpg'
+  },
 
-{
-  food: '딤섬',
-  img: './photo/딤섬.jpg'
-},
+  {
+    food: '딤섬',
+    img: './photo/딤섬.jpg'
+  },
 
-{
-  food: '라조기',
-  img: './photo/라조기.jpg'
-},
+  {
+    food: '라조기',
+    img: './photo/라조기.jpg'
+  },
 
-{
-  food: '마라탕',
-  img: './photo/마라탕.jpg'
-},
+  {
+    food: '마라탕',
+    img: './photo/마라탕.jpg'
+  },
 
-{
-  food: '유린기',
-  img: './photo/유린기.jpg'
-},
+  {
+    food: '유린기',
+    img: './photo/유린기.jpg'
+  },
 
-{
-  food: '오향장육',
-  img: './photo/오향장육.jpg'
-},
+  {
+    food: '오향장육',
+    img: './photo/오향장육.jpg'
+  },
 
-{
-  food: '훠궈',
-  img: './photo/훠궈.jpg'
-},
+  {
+    food: '훠궈',
+    img: './photo/훠궈.jpg'
+  },
 
-{
-  food: '취두부',
-  img: './photo/취두부.jpg'
-},
+  {
+    food: '취두부',
+    img: './photo/취두부.jpg'
+  },
 
-{
-  food: '팔보채',
-  img: './photo/팔보채.jpg'
-},
+  {
+    food: '팔보채',
+    img: './photo/팔보채.jpg'
+  },
 
-{
-  food: '유타오',
-  img: './photo/유타오.jpg'
-},
+  {
+    food: '유타오',
+    img: './photo/유타오.jpg'
+  },
 
-{
-  food: '어향육사',
-  img: './photo/어향육사.jpg'
-},
+  {
+    food: '어향육사',
+    img: './photo/어향육사.jpg'
+  },
 
-{
-  food: '월병',
-  img: './photo/월병.jpg'
-},
+  {
+    food: '월병',
+    img: './photo/월병.jpg'
+  },
 
-{
-  food: '젠빙',
-  img: './photo/젠빙.jpg'
-},
+  {
+    food: '젠빙',
+    img: './photo/젠빙.jpg'
+  },
 
-{
-  food: '주량원자',
-  img: './photo/주량원자.jpg'
-},
+  {
+    food: '주량원자',
+    img: './photo/주량원자.jpg'
+  },
 
-{
-  food: '중쯔',
-  img: './photo/중쯔.jpg'
-},
+  {
+    food: '중쯔',
+    img: './photo/중쯔.jpg'
+  },
 
-{
-  food: '샥스핀',
-  img: './photo/샥스핀.jpg'
-},
+  {
+    food: '샥스핀',
+    img: './photo/샥스핀.jpg'
+  },
 
-{
-  food: '마파두부',
-  img: './photo/마파두부.jpg'
-},
+  {
+    food: '마파두부',
+    img: './photo/마파두부.jpg'
+  },
 
-{
-  food: '불도장',
-  img: './photo/불도장.jpg'
-},
+  {
+    food: '불도장',
+    img: './photo/불도장.jpg'
+  },
 
-{
-  food: '방탕후루',
-  img: './photo/방탕후루.jpg'
-},
+  {
+    food: '방탕후루',
+    img: './photo/방탕후루.jpg'
+  },
 
 ];
 
 
 const Japan = [
 
-{
-  food: '라멘',
-  img: './photo/라멘.jpg'
-},
+  {
+    food: '라멘',
+    img: './photo/라멘.jpg'
+  },
 
-{
-  food: '가라아게',
-  img: './photo/가라아게.jpg'
-},
+  {
+    food: '가라아게',
+    img: './photo/가라아게.jpg'
+  },
 
-{
-  food: '고로케',
-  img: './photo/고로케.jpg'
-},
+  {
+    food: '고로케',
+    img: './photo/고로케.jpg'
+  },
 
-{
-  food: '낫토',
-  img: './photo/낫토.jpg'
-},
+  {
+    food: '낫토',
+    img: './photo/낫토.jpg'
+  },
 
-{
-  food: '돈부리',
-  img: './photo/돈부리.jpg'
-},
+  {
+    food: '돈부리',
+    img: './photo/돈부리.jpg'
+  },
 
-{
-  food: '스키야키',
-  img: './photo/스키야키.jpg'
-},
+  {
+    food: '스키야키',
+    img: './photo/스키야키.jpg'
+  },
 
-{
-  food: '소바',
-  img: './photo/소바.jpg'
-},
+  {
+    food: '소바',
+    img: './photo/소바.jpg'
+  },
 
-{
-  food: '스시',
-  img: './photo/스시.jpg'
-},
+  {
+    food: '스시',
+    img: './photo/스시.jpg'
+  },
 
-{
-  food: '야키토리',
-  img: './photo/야키토리.jpg'
-},
+  {
+    food: '야키토리',
+    img: './photo/야키토리.jpg'
+  },
 
-{
-  food: '타코야끼',
-  img: './photo/타코야끼.jpg'
-},
+  {
+    food: '타코야끼',
+    img: './photo/타코야끼.jpg'
+  },
 
-{
-  food: '우메보시',
-  img: './photo/우메보시.jpg'
-},
-
-
-{
-  food: '우동',
-  img: './photo/우동.jpg'
-},
+  {
+    food: '우메보시',
+    img: './photo/우메보시.jpg'
+  },
 
 
-{
-  food: '야키니쿠',
-  img: './photo/야키니쿠.jpg'
-},
+  {
+    food: '우동',
+    img: './photo/우동.jpg'
+  },
 
 
-{
-  food: '규동',
-  img: './photo/규동.jpg'
-},
+  {
+    food: '야키니쿠',
+    img: './photo/야키니쿠.jpg'
+  },
+
+
+  {
+    food: '규동',
+    img: './photo/규동.jpg'
+  },
 ];
 const Western = [
 
-{
-  food: '파스타',
-  img: './photo/파스타.jpg'
-},
+  {
+    food: '파스타',
+    img: './photo/파스타.jpg'
+  },
 
-{
-  food: '피쉬앤칩스',
-  img: './photo/피쉬앤칩스.jpg'
-},
+  {
+    food: '피쉬앤칩스',
+    img: './photo/피쉬앤칩스.jpg'
+  },
 
-{
-  food: '햄버거',
-  img: './photo/햄버거.jpg'
-},
+  {
+    food: '햄버거',
+    img: './photo/햄버거.jpg'
+  },
 
-{
-  food: '피자',
-  img: './photo/피자.jpg'
-},
+  {
+    food: '피자',
+    img: './photo/피자.jpg'
+  },
 
-{
-  food: '리조또',
-  img: './photo/리조또.jpg'
-},
+  {
+    food: '리조또',
+    img: './photo/리조또.jpg'
+  },
 
-{
-  food: '미트볼',
-  img: './photo/미트볼.jpg'
-},
+  {
+    food: '미트볼',
+    img: './photo/미트볼.jpg'
+  },
 
-{
-  food: '아란치니',
-  img: './photo/아란치니.jpg'
-},
+  {
+    food: '아란치니',
+    img: './photo/아란치니.jpg'
+  },
 
-{
-  food: '에그인헬',
-  img: './photo/에그인헬.jpg'
-},
+  {
+    food: '에그인헬',
+    img: './photo/에그인헬.jpg'
+  },
 
-{
-  food: '폴렌타',
-  img: './photo/폴렌타.jpg'
-},
+  {
+    food: '폴렌타',
+    img: './photo/폴렌타.jpg'
+  },
 
-{
-  food: '뇨끼',
-  img: './photo/뇨끼.jpg'
-},
+  {
+    food: '뇨끼',
+    img: './photo/뇨끼.jpg'
+  },
 
-{
-  food: '맥앤치즈',
-  img: './photo/맥앤치즈.jpg'
-},
+  {
+    food: '맥앤치즈',
+    img: './photo/맥앤치즈.jpg'
+  },
 
-{
-  food: '핫도그',
-  img: './photo/핫도그.jpg'
-},
+  {
+    food: '핫도그',
+    img: './photo/핫도그.jpg'
+  },
 
-{
-  food: '라쟈냐',
-  img: './photo/라쟈냐.jpg'
-},
+  {
+    food: '라쟈냐',
+    img: './photo/라쟈냐.jpg'
+  },
 
 ];
 
@@ -410,175 +410,208 @@ const $imgbox = document.querySelector('.imgbox');
 var timeSurv;
 
 function makeGameData() {
-const $timeSelect = document.querySelector('.timeSelect');
+  const $timeSelect = document.querySelector('.timeSelect');
 
-return {
-  timeSelect: $timeSelect.onchange = () => {
-    const $timeSelect = document.querySelector('.timeSelect').value;
-    return $timeSelect;
-  }, //선택된 제한시간
-  timeImgSecond: 3,
-  timeSecond: null,
-  score: 0,
+  return {
+    timeSelect: $timeSelect.onchange = () => {
+      const $timeSelect = document.querySelector('.timeSelect').value;
+      return $timeSelect;
+    }, //선택된 제한시간
+    timeImgSecond: 3,
+    timeSecond: null,
+    score: 0,
 
-};
+  };
 }
 
 
 $level.onclick = function () {
-console.log('kllll');
-// if($catalog.attributes('style') == 'none' ){
-// }
-$catalog.forEach(($li) => {
-  $li.style.visibility = 'visible';
-});
+  console.log('kllll');
+  // if($catalog.attributes('style') == 'none' ){
+  // }
+  $catalog.forEach(($li) => {
+    $li.style.visibility = 'visible';
+  });
 
 }
 
 $catalog.forEach(($btn) => {
+  // console.log($btn);
+  $btn.onclick = function () {
+    $catalog.forEach(($li) => {
+      $li.style.cssText = `visibility = hidden; z-index: -1;`;
+      $catalog_2.style.cssText = `z-index : -1;`;
 
-// console.log($btn);
-$btn.onclick = function () {
-  $catalog.forEach(($li) => {
-    $li.style.cssText = `visibility = hidden; z-index: -1;`;
-    $catalog_2.style.cssText = `z-index : -1;`;
+    });
 
-  });
+    if ($btn.getAttribute('id') === 'korea') {
+      selectedCourse = korea;
+      console.log(selectedCourse);
 
-  if ($btn.getAttribute('id') === 'korea') {
-    selectedCourse = korea;
-    console.log(selectedCourse);
-    // imgMatch(selectedCourse);
 
-  } else if ($btn.getAttribute('id') === 'japan') {
-    selectedCourse = Japan;
-    console.log(selectedCourse);
-    // imgMatch(selectedCourse);
+    } else if ($btn.getAttribute('id') === 'japan') {
+      selectedCourse = Japan;
+      console.log(selectedCourse);
+   
 
-  } else if ($btn.getAttribute('id') === 'western') {
-    selectedCourse = Western;
-    console.log(selectedCourse);
-    // imgMatch(selectedCourse);
+    } else if ($btn.getAttribute('id') === 'western') {
+      selectedCourse = Western;
+      console.log(selectedCourse);
 
-  } else if ($btn.getAttribute('id') === 'total') {
-    selectedCourse = korea.concat(Japan, China, Western);
-    console.log(selectedCourse);
-    // imgMatch(selectedCourse);
 
-  } else {
-    selectedCourse = China;
-    console.log(selectedCourse);
-    // imgMatch(selectedCourse);
+    } else if ($btn.getAttribute('id') === 'total') {
+      selectedCourse = korea.concat(Japan, China, Western);
+      console.log(selectedCourse);
+    
+    } else {
+      selectedCourse = China;
+      console.log(selectedCourse);
 
+    }
+    return false;
   }
-  return false;
-}
 });
+
 startGameBtn();
 
 function startGameBtn() {
-let timeImgSecond = 3;
+  let timeImgSecond = 3;
 
-startBtn.onclick = function () { // start버튼 누르면 사라지는 함수
-  // let $time = +makeGameData().timeSelect();
-  startBtn.classList.add('hidden'); // start버튼 숨기기
-  $timer.style.display = 'block'; // 남은시간 보여주기
+  startBtn.onclick = function () { // start버튼 누르면 사라지는 함수
+    // let $time = +makeGameData().timeSelect();
+    startBtn.classList.add('hidden'); // start버튼 숨기기
+    $timer.style.display = 'block'; // 남은시간 보여주기
 
-  let timeImgStart = timeImgSecond;
-  let timeImg = setInterval(function () { // 사진박스 3,2,1 카운트 다운
-    $imgbox.textContent = timeImgStart;
-    $imgbox.setAttribute('value', timeImgStart);
-    timeImgStart--;
-    if (timeImgStart === -1) { // 0초가 되면 랜덤 음식 사진 보여주기
-      $imgbox.textContent = '';
-      clearInterval(timeImg); // 0초가 되면 함수 종료
-    }
-    return timeImgStart;
-  }, 1000); // 사진박스 카운트 다운 함수 end
+    let timeImgStart = timeImgSecond;
+    let timeImg = setInterval(function () { // 사진박스 3,2,1 카운트 다운
+      $imgbox.textContent = timeImgStart;
+      $imgbox.setAttribute('value', timeImgStart);
+      timeImgStart--;
+      if (timeImgStart === -1) { // 0초가 되면 랜덤 음식 사진 보여주기
+        $imgbox.textContent = '';
+        clearInterval(timeImg); // 0초가 되면 함수 종료
+      }
+      return timeImgStart;
+    }, 1000); // 사진박스 카운트 다운 함수 end
 
-  setTimeout(()=>{
-    let $newImg = document.createElement('img');
+    setTimeout(() => {
+      let $newImg = document.createElement('img');
       $newImg.setAttribute('id', 'foodimg');
       $imgbox.appendChild($newImg);
       imgMatch(selectedCourse); // 랜덤 음식 사진 보여주는 함수
-    },4000);
-    setTimeout(()=>{
-    timeAttack();
-  },3000);
-  
-}
+    }, 4000);
+    setTimeout(() => {
+      timeAttack();
+    }, 3000);
+
+  }
 }
 //10번 제한 하는 변수
 
 
 function imgMatch(selectedCourse) {
-//사진 랜덤 함수
-//이미지랜덤 숫자
-// const imgNum = randomFood();
-//이미지 보이기 
-// let objImg = document.getElementById('foodimg');
-// objImg.setAttribute('src', selectedFood);
+  //사진 랜덤 함수
+  //이미지랜덤 숫자
+  // const imgNum = randomFood();
+  //이미지 보이기 
+  // let objImg = document.getElementById('foodimg');
+  // objImg.setAttribute('src', selectedFood);
 
-const random = rdimg();
-const selectedFood = selectedCourse[random].img;
-var arrUrl = selectedFood.split("/");
-var FileNameplus = arrUrl[arrUrl.length - 1]; //나누어진 배열의 맨 끝이 파일명
-var arSplitFileName = FileNameplus.split("."); //파일명을 다시 "."로 나누어 확장자랑 쪼갬
-const $inputAnswer = document.getElementById('answer');
-var FileName = arSplitFileName[0]; //파일이름
+  const random = rdimg();
+  const selectedFood = selectedCourse[random].img;
+  var arrUrl = selectedFood.split("/");
+  var FileNameplus = arrUrl[arrUrl.length - 1]; //나누어진 배열의 맨 끝이 파일명
+  var arSplitFileName = FileNameplus.split("."); //파일명을 다시 "."로 나누어 확장자랑 쪼갬
+  const $inputAnswer = document.getElementById('answer');
+  var FileName = arSplitFileName[0]; //파일이름
 
-$inputAnswer.addEventListener('keyup', (e) => {
-  if (e.key === 'Enter') {
-    if (FileName === document.getElementById('answer').value) {
-      corrected();
-      // timeAttack();
-    } else {
-      failed();
-      // timeAttack();
+  $inputAnswer.onkeyup= (e) => {
+    if (e.key === 'Enter') {
+      if (FileName === document.getElementById('answer').value) {
+        console.log(`${score}   10회미만1`);
+        corrected();
+        // timeAttack();
+      } else {
+        console.log(`${score} 답이 틀릴때의 경우`);
+        failed();
+        // timeAttack();
+      }
     }
-  }
 
-});
+  };
 }
 
 function rdimg() {
-let foodimg = Math.round(Math.random() * selectedCourse.length);
-// console.log(foodimg); // 랜덤이미지 생성
-let objImg = document.getElementById('foodimg');
-objImg.setAttribute('src', selectedCourse[foodimg].img);
-return foodimg;
+  let foodimg = Math.round(Math.random() * selectedCourse.length);
+  // console.log(foodimg); // 랜덤이미지 생성
+  let objImg = document.getElementById('foodimg');
+  objImg.setAttribute('src', selectedCourse[foodimg].img);
+  return foodimg;
 
 }
 
 
 function corrected() {
-$correct.textContent = '정답입니다!!';
-score++;
-$score.style.width = '100px';
-$score.textContent = ': ' + score + ' 점';
-$inputAnswer.value = '';
-//10회 미만으로 맞췄을때 실행
-if(score <= 10){
-  imgMatch(selectedCourse);
-  // startGameBtn();
-  clearInterval(timeSurv);
-  timeAttack();
-} else{
-    alert(`당신은 엄청난 돼지입니다!!!!!!으아갸갸갹!!!!`)
+  $correct.textContent = '정답입니다!!';
+  score++;
+  $score.style.width = '100px';
+  $score.textContent = ': ' + score + ' 점';
+  $inputAnswer.value = '';
+  //10회 미만으로 맞췄을때 실행
+  if (score < 5) {
+    console.log(`${score}   10회미만`);
+    imgMatch(selectedCourse);
+    // startGameBtn();
+    clearInterval(timeSurv);
+    timeAttack();
+  } 
+  else if (score >= 5) {
+    
+    console.log('????>/./...??');
+    var retry = confirm(`10개 맞추기 성공!! 다시하시겠습니까?`);
+    $score.textContent = '';
+    if (retry === true) {
+      $catalog.forEach(($li) => {
+        $li.style.visibility = 'visible';
+        $li.style.cssText = `visibility = visible; z-index: 100;`;
+        
+      });
+      score = 0;
+      $inputAnswer.value = '';
+      imgMatch(selectedCourse);
+      startGameBtn();
+    } else {
+      alert(`수고하셨습니다!!`);
+    }
+  }
 }
-}
+
+
+
 
 function failed() {
-$score.innerHTML = '땡!!!!<br>' + '점수: ' + score + '점';
+  $score.innerHTML = '땡!!!!<br>' + '점수: ' + score + '점';
+  $score.style.cssText = `z-index = 100; font-weight: 700; font-size: 40px; background:white;`;
+  console.log(`${score}????>/./...??fail`);
+  if (confirm(`다시하시겠습니까?`)) {
+    score = 0;
+    imgMatch(selectedCourse);
+    startGameBtn();
+  } else {
 
-// $correct.textContent = '땡!!';
-$score.style.cssText = `z-index = 100; font-weight: 700; font-size: 40px; background:white;`;
+    alert(`멍충멍충!!`);
+    var link = "../돼지테스트/chooseone.html";
+    location.href = link;
+    location.replace(link);
+    window.open(link);
+  }
+  $correct.textContent = '땡!!';
 }
 
-function timeAttack(){
-const $time = makeGameData().timeSelect();
-let tim = $time;
-// 1초씩 남은시간 깍이는 함수
+function timeAttack() {
+  const $time = makeGameData().timeSelect();
+  let tim = $time;
+  // 1초씩 남은시간 깍이는 함수
   timeSurv = setInterval(function () { // 난이도에 따라 타이머 설정
     $timer.setAttribute('value', tim);
     tim--;
