ebe52d094bbac79f923c449c72a9445c4d201c29
1gizmo <126743255+1gizmo@users.noreply.github.com>
2023-04-01 17:23:56 +0900

    수도게임자료추가

---
+++ 수도보고 나라이름 맞추기/수도배열.js
@@ -0,0 +1,67 @@
+const countries = [
+
+    { country: '미국', capital: '워싱턴 D.C.' },
+    
+    { country: '캐나다', capital: '오타와' },
+    
+    { country: '멕시코', capital: '멕시코시' },
+    
+    { country: '브라질', capital: '브라질리아' },
+    
+    { country: '아르헨티나', capital: '부에노스아이레스' },
+    
+    { country: '칠레', capital: '산티아고' },
+    
+    { country: '콜롬비아', capital: '보고타' },
+
+    { country: '온두라스', capital: '테구시갈파' },
+    
+    { country: '자메이카', capital: '킹스턴' },
+    
+    { country: '페루', capital: '리마' },
+    
+    { country: '영국', capital: '런던' },
+    
+    { country: '프랑스', capital: '파리' },
+    
+    { country: '독일', capital: '베를린' },
+    
+    { country: '이탈리아', capital: '로마' },
+    
+    { country: '스페인', capital: '마드리드' },
+    
+    { country: '포르투갈', capital: '리스본' },
+    
+    { country: '네덜란드', capital: '암스테르담' },
+    
+    { country: '스위스', capital: '베른' },
+    
+    { country: '벨기에', capital: '브뤼셀' },
+    
+    { country: '오스트리아', capital: '빈' },
+    
+    { country: '중국', capital: '북경' },
+    
+    { country: '일본', capital: '도쿄' },
+    
+    { country: '인도', capital: '뉴델리' },
+    
+    { country: '파키스탄', capital: '이슬라마바드' },
+    
+    { country: '인도네시아', capital: '자카르타' },
+    
+    { country: '태국', capital: '방콕' },
+    
+    { country: '말레이시아', capital: '쿠알라룸푸르' },
+    
+    { country: '싱가포르', capital: '싱가포르' },
+    
+    { country: '필리핀', capital: '마닐라' },
+    
+    { country: '베트남', capital: '하노이' },
+    
+    { country: '이집트', capital: '카이로' },
+    
+    { country: '나이지리아', capital: '아부자' }
+    
+];
