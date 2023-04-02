7d2542e4e6b41eb81f7bea80094f0243a4cd7427
1gizmo <jdonggwan@naver.com>
2023-04-02 15:10:42 +0900

    수도보고 나라맞추기

---
+++ 수도보고 나라이름 맞추기/country.js
@@ -0,0 +1,947 @@
+const country = [
+  {
+    country: '가나',
+    img: './photo/가나.jpg'
+
+  },
+  {
+    country: '그리스',
+    img: './photo/그리스.jpg'
+
+  },
+  {
+    country: '나마비아',
+    img: './photo/나마비아.jpg'
+
+  },
+  {
+    country: '나이지리아',
+    img: './photo/나이지리아.jpg'
+
+  },
+  {
+    country: '남아프리카공화국',
+    img: './photo/남아프리카공화국.jpg'
+
+  },
+  {
+    country: '네덜란드',
+    img: './photo/네덜란드.jpg'
+
+  },
+  {
+    country: '네팔',
+    img: './photo/네팔.jpg'
+
+  },
+  {
+    country: '노르웨이',
+    img: './photo/노르웨이.jpg'
+
+  },
+  {
+    country: '덴마크',
+    img: './photo/덴마크.jpg'
+
+  },
+  {
+    country: '독일',
+    img: './photo/독일.jpg'
+
+  },
+  {
+    country: '러시아',
+    img: './photo/러시아.jpg'
+
+  },
+  {
+    country: '말레이시아',
+    img: './photo/말레이시아.jpg'
+
+  },
+  {
+    country: '멕시코',
+    img: './photo/멕시코.jpg'
+
+  },
+  {
+    country: '모로코',
+    img: './photo/모로코.jpg'
+
+  },
+  {
+    country: '미국',
+    img: './photo/미국.jpg'
+
+  },
+  {
+    country: '방글라데시',
+    img: './photo/방글라데시.jpg'
+
+  },
+  {
+    country: '베트남',
+    img: './photo/베트남.jpg'
+
+  },
+  {
+    country: '벨기에',
+    img: './photo/벨기에.jpg'
+
+  },
+  {
+    country: '벨라루스',
+    img: './photo/벨라루스.jpg'
+
+  },
+  {
+    country: '브라질',
+    img: './photo/브라질.jpg'
+
+  },
+  {
+    country: '사우디아라비아',
+    img: './photo/사우디아라비아.jpg'
+
+  },
+  {
+    country: '스웨덴',
+    img: './photo/스웨덴.jpg'
+
+  },
+  {
+    country: '스위스',
+    img: './photo/스위스.jpg'
+
+  },
+  {
+    country: '스폐인',
+    img: './photo/스폐인.jpg'
+
+  },
+  {
+    country: '싱가포르',
+    img: './photo/싱가포르.jpg'
+
+  },
+  {
+    country: '아랍에미리트',
+    img: './photo/아랍에미리트.jpg'
+
+  },
+  {
+    country: '아르헨티나',
+    img: './photo/아르헨티나.jpg'
+
+  },
+  {
+    country: '에티오피아',
+    img: './photo/에티오피아.jpg'
+
+  },
+  {
+    country: '영국',
+    img: './photo/영국.jpg'
+
+  },
+  {
+    country: '오만',
+    img: './photo/오만.jpg'
+
+  },
+  {
+    country: '오스트리아',
+    img: './photo/오스트리아.jpg'
+
+  },
+  {
+    country: '온두라스',
+    img: './photo/온두라스.jpg'
+
+  },
+  {
+    country: '우간다',
+    img: './photo/우간다.jpg'
+
+  },
+  {
+    country: '우크라이나',
+    img: './photo/우크라이나.jpg'
+
+  },
+  {
+    country: '이라크',
+    img: './photo/이라크.jpg'
+
+  },
+  {
+    country: '이란',
+    img: './photo/이란.jpg'
+
+  },
+  {
+    country: '이스라엘',
+    img: './photo/이스라엘.jpg'
+
+  },
+  {
+    country: '이집트',
+    img: './photo/이집트.jpg'
+
+  },
+  {
+    country: '이탈리아',
+    img: './photo/이탈리아.jpg'
+
+  },
+  {
+    country: '인도',
+    img: './photo/인도.jpg'
+
+  },
+  {
+    country: '인도네시아',
+    img: './photo/인도네시아.jpg'
+    
+  },
+  {
+    country: '인도스타나',
+    img: './photo/인도스타나.jpg'
+
+  },
+  {
+    country: '일본',
+    img: './photo/일본.jpg'
+
+  },
+  {
+    country: '자메이카',
+    img: './photo/자메이카.jpg'
+
+  },
+  {
+    country: '중국',
+    img: './photo/중국.jpg'
+
+  },
+  {
+    country: '체코',
+    img: './photo/체코.jpg'
+
+  },
+  {
+    country: '칠레',
+    img: './photo/칠레.jpg'
+
+  },
+  {
+    country: '카타르',
+    img: './photo/카타르.jpg'
+
+  },
+  {
+    country: '캐나다',
+    img: './photo/캐나다.jpg'
+
+  },
+  {
+    country: '케냐',
+    img: './photo/케냐.jpg'
+
+  },
+  {
+    country: '코트디부아르',
+    img: './photo/코트디부아르.jpg'
+
+  },
+  {
+    country: '크로아티아',
+    img: './photo/크로아티아.jpg'
+
+  },
+  {
+    country: '태국',
+    img: './photo/태국.jpg'
+
+  },
+  {
+    country: '터키',
+    img: './photo/터키.jpg'
+
+  },
+  {
+    country: '토고',
+    img: './photo/토고.jpg'
+
+  },
+  {
+    country: '파키스탄',
+    img: './photo/파키스탄.jpg'
+
+  },
+  {
+    country: '페루',
+    img: './photo/페루.jpg'
+
+  },
+  {
+    country: '포루투칼',
+    img: './photo/포루투칼.jpg'
+
+  },
+  {
+    country: '폴란드',
+    img: './photo/폴란드.jpg'
+
+  },
+  {
+    country: '프랑스',
+    img: './photo/프랑스.jpg'
+
+  },
+  {
+    country: '핀란드',
+    img: './photo/핀란드.jpg'
+
+  },
+  {
+    country: '필리핀',
+    img: './photo/필리핀.jpg'
+
+  },
+  {
+    country: '헝가리',
+    img: './photo/헝가리.jpg'
+
+  },
+];
+
+const easy = [
+
+  
+    {
+      country: '중국',
+      img: './photo/중국.jpg'
+  
+    },
+    {
+      country: '베트남',
+      img: './photo/베트남.jpg'
+  
+    }, 
+   {
+      country: '미국',
+      img: './photo/미국.jpg'
+  
+    },
+   {
+      country: '영국',
+      img: './photo/영국.jpg'
+  
+    },
+    {
+      country: '이집트',
+      img: './photo/이집트.jpg'
+  
+    },
+    {
+      country: '일본',
+      img: './photo/일본.jpg'
+  
+    },
+    {
+      country: '캐나다',
+      img: './photo/캐나다.jpg'
+  
+    },
+    {
+      country: '프랑스',
+      img: './photo/프랑스.jpg'
+  
+    },
+    
+];
+
+const normal = [
+  {
+    country: '코트디부아르',
+    img: './photo/코트디부아르.jpg'
+
+  },
+  {
+    country: '크로아티아',
+    img: './photo/크로아티아.jpg'
+
+  },
+  {
+    country: '태국',
+    img: './photo/태국.jpg'
+
+  },
+  {
+    country: '터키',
+    img: './photo/터키.jpg'
+
+  },
+  {
+    country: '토고',
+    img: './photo/토고.jpg'
+
+  },
+  {
+    country: '파키스탄',
+    img: './photo/파키스탄.jpg'
+
+  },
+  {
+    country: '페루',
+    img: './photo/페루.jpg'
+
+  },
+  {
+    country: '포루투칼',
+    img: './photo/포루투칼.jpg'
+
+  },
+  {
+    country: '폴란드',
+    img: './photo/폴란드.jpg'
+
+  },
+  {
+    country: '프랑스',
+    img: './photo/프랑스.jpg'
+
+  },
+  {
+    country: '핀란드',
+    img: './photo/핀란드.jpg'
+
+  },
+  {
+    country: '필리핀',
+    img: './photo/필리핀.jpg'
+
+  },
+  {
+    country: '헝가리',
+    img: './photo/헝가리.jpg'
+
+  },
+
+];
+
+const hard = [
+
+  {
+    country: '나마비아',
+    img: './photo/나마비아.jpg'
+
+  },
+  {
+    country: '나이지리아',
+    img: './photo/나이지리아.jpg'
+
+  },
+  {
+    country: '남아프리카공화국',
+    img: './photo/남아프리카공화국.jpg'
+
+  },
+  {
+    country: '네덜란드',
+    img: './photo/네덜란드.jpg'
+
+  },
+  {
+    country: '네팔',
+    img: './photo/네팔.jpg'
+
+  },
+  {
+    country: '노르웨이',
+    img: './photo/노르웨이.jpg'
+
+  },
+  {
+    country: '덴마크',
+    img: './photo/덴마크.jpg'
+
+  },
+  {
+    country: '독일',
+    img: './photo/독일.jpg'
+
+  },
+  {
+    country: '러시아',
+    img: './photo/러시아.jpg'
+
+  },
+  {
+    country: '말레이시아',
+    img: './photo/말레이시아.jpg'
+
+  },
+  {
+    country: '멕시코',
+    img: './photo/멕시코.jpg'
+
+  },
+  {
+    country: '모로코',
+    img: './photo/모로코.jpg'
+
+  },
+  {
+    country: '미국',
+    img: './photo/미국.jpg'
+
+  },
+  {
+    country: '방글라데시',
+    img: './photo/방글라데시.jpg'
+
+  },
+  {
+    country: '베트남',
+    img: './photo/베트남.jpg'
+
+  },
+  {
+    country: '벨기에',
+    img: './photo/벨기에.jpg'
+
+  },
+  {
+    country: '벨라루스',
+    img: './photo/벨라루스.jpg'
+
+  },
+  {
+    country: '브라질',
+    img: './photo/브라질.jpg'
+
+  },
+  {
+    country: '사우디아라비아',
+    img: './photo/사우디아라비아.jpg'
+
+  },
+  {
+    country: '스웨덴',
+    img: './photo/스웨덴.jpg'
+
+  },
+  {
+    country: '스위스',
+    img: './photo/스위스.jpg'
+
+  },
+  {
+    country: '스폐인',
+    img: './photo/스폐인.jpg'
+
+  },
+  {
+    country: '싱가포르',
+    img: './photo/싱가포르.jpg'
+
+  },
+  {
+    country: '아랍에미리트',
+    img: './photo/아랍에미리트.jpg'
+
+  },
+  {
+    country: '아르헨티나',
+    img: './photo/아르헨티나.jpg'
+
+  },
+  {
+    country: '에티오피아',
+    img: './photo/에티오피아.jpg'
+
+  },
+  {
+    country: '영국',
+    img: './photo/영국.jpg'
+
+  },
+  {
+    country: '오만',
+    img: './photo/오만.jpg'
+
+  },
+  {
+    country: '오스트리아',
+    img: './photo/오스트리아.jpg'
+
+  },
+  {
+    country: '온두라스',
+    img: './photo/온두라스.jpg'
+
+  },
+  {
+    country: '우간다',
+    img: './photo/우간다.jpg'
+
+  },
+  {
+    country: '우크라이나',
+    img: './photo/우크라이나.jpg'
+
+  },
+  {
+    country: '이라크',
+    img: './photo/이라크.jpg'
+
+  },
+  {
+    country: '이란',
+    img: './photo/이란.jpg'
+
+  },
+  {
+    country: '이스라엘',
+    img: './photo/이스라엘.jpg'
+
+  },
+  {
+    country: '이집트',
+    img: './photo/이집트.jpg'
+
+  },
+  {
+    country: '이탈리아',
+    img: './photo/이탈리아.jpg'
+
+  },
+  {
+    country: '인도',
+    img: './photo/인도.jpg'
+
+  },
+  {
+    country: '인도네시아',
+    img: './photo/인도네시아.jpg'
+    
+  },
+  {
+    country: '인도스타나',
+    img: './photo/인도스타나.jpg'
+
+  },
+
+];
+
+
+
+
+
+
+const $level = document.querySelector('.levelselect'); //코스 선택하면 카테고리 보이기
+const $catalog = [...document.querySelectorAll('.kind li button')];
+const $catalog_2 = document.querySelector('.kind');
+let selectedCourse = []; // 코스 선택
+
+let count = 0;
+const $score = document.querySelector('.score');
+const $correct = document.querySelector('.correct'); //정답유무 표시란
+
+const $inputAnswer = document.getElementById('answer');
+let score = makeGameData().score;
+
+const startBtn = document.querySelector('.start');
+const $timer = document.querySelector('.timer');
+const $imgbox = document.querySelector('.imgbox');
+const $realimg = document.querySelector('.imgbox .randomimg');
+const $pig = document.querySelector('.eatingPig'); //포크를든 돼지
+
+//fade 효과 적용하기 
+let $fade = document.querySelector('.fadeffect');
+
+
+
+var timeSurv;
+let countLife = 3;
+
+const $pigLife = document.querySelectorAll('.piglife');
+const $crownPig = document.querySelector('.animated-entity-symbol');
+const $boomPig = document.querySelector('.animated-entity-symbol2');
+const $pigs = document.querySelector('.pigs');
+
+
+function makeGameData() {
+    const $timeSelect = document.querySelector('.timeSelect');
+
+    return {
+      timeSelect: $timeSelect.onchange = () => {
+        const $timeSelect = document.querySelector('.timeSelect').value;
+        return $timeSelect;
+      }, //선택된 제한시간
+      timeImgSecond: 3,
+      timeSecond: null,
+      score: 0,
+    };
+}
+
+
+$level.onclick = function () { // 코스 선택
+  $catalog.forEach(($li) => { // 코스들 보이기
+    $li.style.visibility = 'visible';
+  });
+}
+
+$catalog.forEach(($btn) => { // 선택해야할 코스들
+
+  $btn.onclick = function () {
+    $catalog.forEach(($li) => {
+      $li.style.cssText = `visibility = hidden; z-index: -1;`;
+      $catalog_2.style.cssText = `z-index : -1;`;
+
+    });
+
+    if ($btn.getAttribute('id') === 'country') {
+      selectedCourse = country;
+      console.log(selectedCourse);
+      // imgMatch(selectedCourse);
+
+    } else if ($btn.getAttribute('id') === 'easy') {
+      selectedCourse = easy;
+      console.log(selectedCourse);
+      // imgMatch(selectedCourse);
+
+    } else if ($btn.getAttribute('id') === 'normal') {
+      selectedCourse = normal;
+      console.log(selectedCourse);
+      // imgMatch(selectedCourse);
+
+    } else if ($btn.getAttribute('id') === 'hard') {
+      selectedCourse = hard;
+      console.log(selectedCourse);
+      // imgMatch(selectedCourse);
+
+    } else {
+      selectedCourse = country;
+      console.log(selectedCourse);
+      // imgMatch(selectedCourse);
+
+    }
+    return false;
+  }
+});
+startGameBtn();
+
+function startGameBtn() {
+  let timeImgSecond = 3;
+
+  startBtn.onclick = function () { // start버튼 누르면 사라지는 함수
+    startBtn.classList.add('hidden'); // start버튼 숨기기
+    $pigs.classList.add('show');
+    $timer.style.display = 'block'; // 남은시간 보여주기
+    $level.disabled = true;
+    const $timeSelectt = document.querySelector('.timeSelect')
+    $timeSelectt.disabled = true;
+
+    window.scrollTo({ // 시작버튼 누르면 스크롤 자동으로 내려주기
+      top: document.body.scrollHeight, // <- 페이지 총 Height
+      behavior: "smooth"
+    });
+
+
+
+    let timeImgStart = timeImgSecond;
+    let timeImg = setInterval(function () { // 사진박스 3,2,1 카운트 다운
+      $imgbox.textContent = timeImgStart;
+      $imgbox.setAttribute('value', timeImgStart);
+      timeImgStart--;
+      if (timeImgStart === -1) { // 0초가 되면 랜덤 음식 사진 보여주기
+        $imgbox.textContent = '';
+        clearInterval(timeImg); // 0초가 되면 함수 종료
+      }
+      return timeImgStart;
+    }, 1000); // 사진박스 카운트 다운 함수 end
+
+    setTimeout(() => { // 처음에 4초뒤 랜덤한 이미지 보여주기
+      let $newImg = document.createElement('img');
+      $newImg.setAttribute('id', 'foodimg');
+      $imgbox.appendChild($newImg);
+      imgMatch(selectedCourse); // 랜덤 음식 사진 보여주는 함수
+    }, 4000);
+    setTimeout(() => { // 3초후 남은시간 카운트다운
+      timeAttack();
+    }, 3000);
+
+  }
+}
+
+
+function imgMatch(selectedCourse) {
+  //사진 랜덤 함수
+  //이미지랜덤 숫자
+  // const imgNum = randomFood();
+  //이미지 보이기 
+  // let objImg = document.getElementById('foodimg');
+  // objImg.setAttribute('src', selectedFood);
+
+  const random = rdimg();
+  const selectedFood = selectedCourse[random].img;
+  var arrUrl = selectedFood.split("/");
+  var FileNameplus = arrUrl[arrUrl.length - 1]; //나누어진 배열의 맨 끝이 파일명
+  var arSplitFileName = FileNameplus.split("."); //파일명을 다시 "."로 나누어 확장자랑 쪼갬
+  const $inputAnswer = document.getElementById('answer');
+  var FileName = arSplitFileName[0]; //파일이름
+
+  $inputAnswer.onkeyup = (e) => {
+    if (e.key === 'Enter') {
+      if (FileName === document.getElementById('answer').value) {
+        corrected();
+
+      } else {
+        $inputAnswer.value = '';
+        --countLife;
+        // console.log($pigLife[countLife]);
+        $pigLife[countLife].remove();
+        $timer.textContent = `남은시간 : ${makeGameData().timeSelect()}`;
+        clearInterval(timeSurv);
+        timeAttack();
+        imgMatch(selectedCourse);
+        console.log(countLife);
+        if (countLife === 0) {
+          $inputAnswer.disabled = true;
+          failed();
+        }
+      }
+    }
+  };
+}
+
+function rdimg() { // 랜덤 숫자 리턴 함수
+  let foodimg = Math.round(Math.random() * selectedCourse.length);
+  // console.log(foodimg); // 랜덤이미지 생성
+  let objImg = document.getElementById('foodimg');
+  objImg.setAttribute('src', selectedCourse[foodimg].img);
+
+  return foodimg;
+}
+
+// const $tooth = document.querySelector('.tooth img');
+
+function corrected() { // 정답일때 나오는 함수
+    //정답일때 돼지가 앙
+  $imgbox.animate([
+    // {transform : 'scale(0.2)'},
+    {
+      transform: 'rotateX(360deg)'
+    },
+    {
+      transform: 'translate(-400px, -300px)'
+    },
+    {
+      transform: 'transition : 1s'
+    },
+    {
+      width: '30px',
+      height: '30px'
+    },
+    {
+      border: 'none',
+      padding: '0'
+    }
+  ], 1000);
+  
+  $pig.animate([
+    {
+      transform: 'rotateX(30deg)'
+    },
+    {
+      transform: 'rotateY(30deg)'
+    },
+    {
+      transform: 'rotateX(30deg)'
+    },
+    {
+      transform: 'rotateY(30deg)'
+    },
+  ], 1000);
+
+
+  $correct.textContent = '정답입니다!!';
+    score++;
+  //   $score.style.width = '100px';
+    $score.textContent = '  ' + score + ' 점';
+    $inputAnswer.value = '';
+    console.log('맞음');
+  
+  //10회 미만으로 맞췄을때 실행
+    if (score < 3) {
+  
+    imgMatch(selectedCourse);
+        $timer.textContent = `남은시간 : ${makeGameData().timeSelect()}`;
+      clearInterval(timeSurv);
+      timeAttack();
+    } else {
+      $crownPig.setAttribute('style',  'z-index: 3; visibility: visible');
+    $timer.textContent = `남은시간 :`;
+    clearInterval(timeSurv);
+    $inputAnswer.readOnly = true;
+      // alert(`당신은 엄청난 돼지입니다!!!!!!으아갸갸갹!!!!`);
+    }
+}
+
+function failed() { // 틀렸을때 나오는 함수
+  let $newImg = document.createElement('img');
+      $newImg.setAttribute('id', 'foodimg');
+      $imgbox.appendChild($newImg);
+      let objImg = document.getElementById('foodimg');
+  objImg.setAttribute('src', '../돼지테스트/기본.png');
+  console.log('땡');
+  $boomPig.setAttribute('style', 'z-index: 3; visibility: visible');
+  // $pigLife[countLife].classList.add('hidden');
+  $inputAnswer.value = '';
+  $score.innerHTML = '땡!!!!<br>' + '점수: ' + score + '점';
+  $timer.textContent = '';
+  clearInterval(timeSurv);
+  // $correct.textContent = '땡!!';
+  $score.style.cssText = `z-index = 100; font-weight: 700; font-size: 40px; background:white;`;
+}
+
+function timeAttack() { // 남은시간 카운트 다운해주는 함수
+  const $time = makeGameData().timeSelect();
+  let tim = $time;
+  // 1초씩 남은시간 깍이는 함수
+  timeSurv = setInterval(function () { // 난이도에 따라 타이머 설정
+    $timer.setAttribute('value', tim);
+    tim--;
+    $timer.textContent = `남은시간 : ${tim}`;
+    // console.log(timeSurv);
+    if (tim === 0) {
+      // $inputAnswer.disabled = true;
+      // $timer.textContent = '';
+      // --countLife;
+      // imgMatch(selectedCourse);
+      // console.log(countLife);
+      // if (countLife === 0) {
+      //   failed();
+      // }
+      $inputAnswer.value = '';
+        --countLife;
+        // console.log($pigLife[countLife]);
+        $pigLife[countLife].remove();
+        $timer.textContent = `남은시간 : ${makeGameData().timeSelect()}`;
+        clearInterval(timeSurv);
+        timeAttack();
+        imgMatch(selectedCourse);
+        console.log(countLife);
+        if (countLife === 0) {
+          $inputAnswer.disabled = true;
+          failed();
+        }
+    }
+    // $inputAnswer.value = ''; // 입력하세요 없애기
+    $inputAnswer.focus(); // 시작 버튼 누르면 input 커서 위치
+
+  }, 1000);
+}
